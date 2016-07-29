de0d41c7f683b25642232b61d8d3eb382ec3d5bf
Glimis <912239196@qq.com>
2016-07-29 17:14:42 +0800

    '2'

--- content_script.js
+++ content_script.js
@@ -1,11 +1,28 @@
-﻿chrome.runtime.onMessage.addListener(
+﻿/**
+ * 抓取数据映射至插件上数据而后丢之另一个页面-。-
+ * 
+ */
+//使用ko框架,此处通过顺序的方式获取录入单元
+var map=['number','name','onhand','taxrate'];
+chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    sendResponse({
-      "price": $('#jd-price').text(),
-      "number":$('#short-share span:eq(1)').text(),
-      "name":$('#name h1').text(),
-      "detail1":$('#product-detail-1').html(),
-      "detail2":$('#product-detail-3').text(),
-      "detail3":$('#product-detail-5').text()
-    })
+    if(request.get){
+        sendResponse({
+          "price": $('#jd-price').text(),
+          "number":$('#short-share span:eq(1)').text(),
+          "name":$('#name h1').text(),
+          "detail1":$('#product-detail-1').html(),
+          "detail2":$('#product-detail-3').text(),
+          "detail3":$('#product-detail-5').text()
+        })
+    }else{
+      var data=request.data;
+      var inputs=$('iframe').contents().find('.form-group input');
+      //数据补充
+      data.onhand=parseInt(Math.random()*1000);//
+      data.taxrate=17
+      for(var index in map){
+        inputs.eq(index).val(data[map[index]])
+      }
+    }
 });
--- popup.js
+++ popup.js
@@ -2,11 +2,11 @@ try{
 	var data=JSON.parse(window.localStorage.getItem('data'));
 	setValue(data)
 }catch(e){
-	
+
 }
 $('#get').click(function(){
 		chrome.tabs.query({active:true},function(tab){
-				chrome.tabs.sendMessage(tab[0].id, {greeting: "hello"}, function(data) {
+				chrome.tabs.sendMessage(tab[0].id, {get: 1}, function(data) {
 						window.localStorage.setItem('data',JSON.stringify(data));
 						setValue(data);
     		});
@@ -14,8 +14,25 @@ $('#get').click(function(){
 })
 
 
+$('#put').click(function(){
+	try{
+		var data=JSON.parse(window.localStorage.getItem('data'));
+		chrome.tabs.query({active:true},function(tab){
+				chrome.tabs.sendMessage(tab[0].id, {put: 1,data:data});
+		})
+	}catch(e){
+
+	}
+})
+
+
+/**
+ * 抓取数据映射至插件上
+ */
 function setValue(data){
 	for(var key in data){
 		$('#'+key).text(data[key])
 	}
 }
+
+
